5e211d04a027b8b762e146854669a1fc0e5f4665
Omkar j <107001176+omkarjninja@users.noreply.github.com>
2024-04-13 01:57:34 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
-
-import '../../App.css'
-import LeftSidebar from '../../Compents/LeftSidebar/LeftSidebar'
-import RightSidebar from '../../Compents/RightSidebar/RightSidebar'
-import HomeMainbar from '../../Compents/HomeMainbar/HomeMainbar'
+import React from "react";
+import { useState, useEffect } from "react";
+import "../../App.css";
+import LeftSidebar from "../../Compents/LeftSidebar/LeftSidebar";
+import RightSidebar from "../../Compents/RightSidebar/RightSidebar";
+import HomeMainbar from "../../Compents/HomeMainbar/HomeMainbar";
 
 const Home = () => {
+  var time = new Date();
+  const hour = time.getHours();
+  const [climate, setClimate] = useState("yo");
+  // if (time.getHours() < 6) {
+  //   // alert("hello");
+  //   setClimate("yos");
+  // } else if (hour < 12) {
+  //   setClimate("hey");
+  // } else {
+  //   setClimate("yo");
+  // }
+  useEffect(() => {
+    if (time.getHours() < 6) {
+      // alert("hello");
+      setClimate("yos");
+    } else if (hour < 18) {
+      setClimate("hey");
+    } else {
+      setClimate("yo");
+    }
+  }, []);
   return (
-    <div className='home-container-1'>
+    <div className="home-container-1">
       <LeftSidebar />
-      <div className='home-container-2'>
-       <HomeMainbar />
-       <RightSidebar />
+      <div className={`home-container-2 ${climate}`}>
+        <HomeMainbar />
+        <RightSidebar />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
